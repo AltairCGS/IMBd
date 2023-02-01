c2d868da49afa21d9026de5236644f5b12a42c98
AltairCGS <jhangalvis422@gmail.com>
2023-02-01 17:55:07 -0500

    Update

--- src/app/main/section-movies/card-movies/card-movies.component.ts
+++ src/app/main/section-movies/card-movies/card-movies.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 
 @Component({
@@ -7,7 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-movies.component.css']
 })
 export class CardMoviesComponent {
+  @Input() titleCard: string = "";
+
   title = 'Card Movies';
+  
 
   constructor() { }
   
--- src/app/main/section-movies/section-movies.component.ts
+++ src/app/main/section-movies/section-movies.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-section-movies',
   templateUrl: './section-movies.component.html',
   styleUrls: ['./section-movies.component.css']
 })
-/*  export class SectionMoviesComponent {
+
+
+export class SectionMoviesComponent {
   title = 'Section Movies';
+
+  listPrueba: string[] = ["texto tarea 1", "texto tarea 2", "texto tarea 3", "texto 4", "texto 5", "texto 6"];
 
   constructor() { }
   
@@ -15,36 +18,105 @@ import { Component, OnInit } from '@angular/core';
     console.log('Section Movies Component');
   }
 
-
-
-}  */
-
-export class SectionMoviesComponent implements OnInit {
-
-  cards = [
-  {
-  image: 'https://via.placeholder.com/160x160',
-  alt: 'Card 1',
-  rating: 4.5,
-  title: 'Título de la tarjeta 1'
-  },
-  {
-  image: 'https://via.placeholder.com/160x160',
-  alt: 'Card 2',
-  rating: 3.5,
-  title: 'Título de la tarjeta 2'
-  },
-  {
-  image: 'https://via.placeholder.com/160x160',
-  alt: 'Card 3',
-  rating: 4,
-  title: 'Título de la tarjeta 3'
-  }
+  movieCollection: Movie[] = [
+    {
+      titleMovie: 'The last of us',
+      thumbImage: 'https://loremflickr.com/g/1200/800/paris',
+      alt: 'Image 1',
+      title: 'Image 1'
+    }, {
+      titleMovie: 'El menú',
+      thumbImage: 'https://loremflickr.com/1200/800/brazil,rio',
+      title: 'Image 2',
+      alt: 'Image 2'
+    }, {
+      titleMovie: 'Glass Onion',
+      thumbImage: 'https://loremflickr.com/1200/800/brazil,rio',
+      title: 'Image 3',
+      alt: 'Image 3'
+    }, {
+      titleMovie: 'Avatar: El camino del agua',
+      thumbImage: 'https://loremflickr.com/1200/800/brazil,rio',
+      title: 'Image 4',
+      alt: 'Image 4'
+    }, {
+      titleMovie: 'Los espiritus de la isla',
+      thumbImage: 'https://loremflickr.com/1200/800/paris,girl/all',
+      title: 'Image 5',
+      alt: 'Image 5'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 6',
+      alt: 'Image 6'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 7',
+      alt: 'Image 7'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 7',
+      alt: 'Image 7'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 7',
+      alt: 'Image 7'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 7',
+      alt: 'Image 7'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 7',
+      alt: 'Image 7'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 7',
+      alt: 'Image 7'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 7',
+      alt: 'Image 7'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 7',
+      alt: 'Image 7'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 7',
+      alt: 'Image 7'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 7',
+      alt: 'Image 7'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 7',
+      alt: 'Image 7'
+    }, {
+      titleMovie: 'Mission Majnu',
+      thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+      title: 'Image 7',
+      alt: 'Image 7'
+    }
   ];
-  
-  constructor() { }
-  
-  ngOnInit() {
-  }
-  
-  }
+
+} 
+
+interface Movie {
+  titleMovie: string;
+  thumbImage: string;
+  alt: string;
+  title: string;
+}
